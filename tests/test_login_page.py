9876d28166c23e554bf15d0a07e72f76346d441f
MashaK04 <masha-kashirina2016@yandex.ru>
2025-06-21 10:54:09 +0300

    добавление тестового класса

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,8 +1,12 @@
 import time
-
+from selenium import webdriver
 from pages.login_page import LoginPage
+
+# тестовый класс
 class TestLoginPage:
     def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
         self.login_page = LoginPage(self.driver)
 
     def test_login_in_account(self):
@@ -10,3 +14,6 @@ class TestLoginPage:
         self.login_page.enter_login("qwerty")
         self.login_page.enter_password("password")
         time.sleep(3)
+
+    def teardown_method(self):
+        self.driver.quit()
