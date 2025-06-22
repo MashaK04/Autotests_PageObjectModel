ad6e1b11301c685b1d04764d54f61a6c7c12e303
MashaK04 <masha-kashirina2016@yandex.ru>
2025-06-22 12:02:23 +0300

    Добавление инкапсуляции локаторов

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,17 +3,17 @@ from selenium.webdriver.ie.webdriver import WebDriver
 
 class BasePage:
 
-    HOST_BUTTON_LOCATOR = ("xpath', '//button[text()='Host']")
-    JOIN_BUTTON_LOCATOR = ("xpath', '//button[text()='Join']")
+    _HOST_BUTTON_LOCATOR = ("xpath', '//button[text()='Host']")
+    _JOIN_BUTTON_LOCATOR = ("xpath', '//button[text()='Join']")
 
     def __init__(self, driver):
         self.driver: WebDriver = driver
 
     def open(self):
-        self.driver.get(self.PAGE_URL)
+        self.driver.get(self._PAGE_URL)
 
     def join(self):
-        self.driver.find_element(*self.JOIN_BUTTON_LOCATOR).click()
+        self.driver.find_element(*self._JOIN_BUTTON_LOCATOR).click()
 
     def host(self):
-        self.driver.find_element(*self.HOST_BUTTON_LOCATOR).click()
+        self.driver.find_element(*self._HOST_BUTTON_LOCATOR).click()
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,17 +1,17 @@
 from pages.base_page import BasePage
 
 class LoginPage(BasePage):
-    PAGE_URL = "https://www.freeconferencecall.com/ru/ru/login"
+    _PAGE_URL = "https://www.freeconferencecall.com/ru/ru/login"
 
-    lOGIN_FIELD_LOCATOR= ("xpath", "//input[@name='email']")
-    PASSWORD_FIELD_LOCATOR =("xpath", "//input[@name='password']")
+    _lOGIN_FIELD_LOCATOR= ("xpath", "//input[@name='email']")
+    _PASSWORD_FIELD_LOCATOR =("xpath", "//input[@name='password']")
 
 
     def enter_login(self, login):
-        login_field = self.driver.find_element(*self.lOGIN_FIELD_LOCATOR)
+        login_field = self.driver.find_element(*self._lOGIN_FIELD_LOCATOR)
         login_field.send_keys(login)
 
 
     def enter_password(self, password):
-        password_field = self.driver.find_element(*self.PASSWORD_FIELD_LOCATOR)
+        password_field = self.driver.find_element(*self._PASSWORD_FIELD_LOCATOR)
         password_field.send_keys(password)
